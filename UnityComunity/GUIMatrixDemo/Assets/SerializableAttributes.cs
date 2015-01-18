2734edfc44df737cfe241f1178aaefdd78063cdc
GadjievAlex <gadjievalex@inbox.ru>
2015-01-19 01:38:06 +0300

    new
    new

---
+++ UnityComunity/GUIMatrixDemo/Assets/SerializableAttributes.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SerializableAttributes : MonoBehaviour 
+{
+    [UnityEngine.SerializeField]
+    public Test testVar = new Test();
+
+    public int value;
+	// Use this for initialization
+	void Start () 
+    {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () 
+    {
+	
+	}
+
+    void OnGUI()
+    {
+        if (GUI.Button(new Rect(10, 10, 150, 100), "I am a button"))
+            print("You clicked the button!");
+
+    }
+}
---
+++ UnityComunity/GUIMatrixDemo/Assets/scripts/SerializableDrawerClassDemo/Test.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using UnityEngine;
+
+[System.Serializable]
+public class Test
+{
+   public int p = 5;
+   public Color c = Color.white;
+}
