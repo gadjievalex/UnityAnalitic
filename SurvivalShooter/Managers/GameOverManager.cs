087acee7d5e2137588a9be5d66b0517111aaaa9d
GadjievAlex <gadjievalex@inbox.ru>
2015-01-18 23:34:04 +0300

    Sources Added
    SurvavelShooter Source Added

---
+++ SurvivalShooter/Managers/GameOverManager.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+
+public class GameOverManager : MonoBehaviour
+{
+    public PlayerHealth playerHealth;
+	public float restartDelay = 5f;
+
+    Animator anim;
+	float restartTime;
+
+
+    void Awake()
+    {
+        anim = GetComponent<Animator>();
+    }
+
+
+    void Update()
+    {
+        if (playerHealth.currentHealth <= 0)
+        {
+            anim.SetTrigger("GameOver");
+
+			restartTime+=Time.deltaTime;
+
+			if(restartTime>=restartDelay)
+			{
+				Application.LoadLevel(Application.loadedLevel);
+			}
+        }
+    }
+}
